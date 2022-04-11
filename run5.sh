a8e5dfe695a174967733377429b555eb0b9d6c24
geekJZY <jiangziyu@tamu.edu>
2022-04-11 13:38:45 -0500

    submit 12

--- run5.sh
+++ run5.sh
@@ -32,4 +32,4 @@ python3 -m torch.distributed.launch --nproc_per_node=${n_gpu} --master_port ${po
 train.py --name cifar100-lr${lr}-pruneStoreAttn${pruneStoreAttn}Act${pruneStoreAct}-fast --learning_rate ${lr} --num_workers 2 --output_dir ${save_dir} \
 --dataset cifar100 --model_type ViT-B_16 --pretrained_dir ${save_dir}/pretrain/ViT-B_16.npz \
 --attn_store_prune --prune_ratio_attn_mat_store ${pruneStoreAttn} --prune_ratio_act_store ${pruneStoreAct} \
---train_batch_size 256 --eval_batch_size 256
+--train_batch_size 128 --eval_batch_size 128
