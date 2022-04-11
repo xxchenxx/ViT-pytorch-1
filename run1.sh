67c5ff46e8888799ab4160ee6f36fff1fe6352b0
geekJZY <jiangziyu@tamu.edu>
2022-04-11 01:55:53 -0500

    submit 3

--- run1.sh
+++ run1.sh
@@ -28,7 +28,7 @@ lr=3e-4
 
 CUDA_VISIBLE_DEVICES=${devices} python3 -m torch.distributed.launch --nproc_per_node=${n_gpu} --master_port ${port}  \
 train.py --name cifar100-lr${lr}-fixmlps --learning_rate ${lr} --num_workers 2 --output_dir ${save_dir} \
---dataset cifar100 --model_type ViT-B_16 --pretrained_dir ${save_dir}/pretrain/ViT-B_16.npz --fix_mlps
+--dataset cifar100 --model_type ViT-B_16 --pretrained_dir ${save_dir}/pretrain/ViT-B_16.npz --fix_mlps &
 
 
 lr=1e-3
