484f5274fe173711371916262c2a17403f0739e2
geekJZY <jiangziyu@tamu.edu>
2022-05-10 23:29:03 -0500

    collect data 2

--- tools/collect_data.py
+++ tools/collect_data.py
@@ -20,7 +20,7 @@ def read_num(saveDir, exp):
         lines = file.read().splitlines()
     bestAcc = -1
     for line in lines[-20:]:
-        # set_trace()
+        set_trace()
         groups = re.match("^Best Accuracy: *([0-9]+\.[0-9]+)%$", line)
         if groups:
             bestAcc = float(groups[1])
